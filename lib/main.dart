e3455b64e8fff74a03cb1d7d9efc2b49557e8bd5
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2023-02-14 20:12:16 +0530

    uderstanding Column, Container, Row, Text and ElevatedButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:personal_expense/transaction.dart';
 import './transaction.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,9 +16,11 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transaction = [
-    Transaction("1001", "new Ball", 1000, DateTime.now()),
-    Transaction("1002", "new FootBall", 1000, DateTime.now()),
-    Transaction("1003", "new Bat", 1000, DateTime.now()),
+    Transaction("1001", "Cricket Ball", 1000, DateTime.now()),
+    Transaction("1002", "Dinner ", 5000, DateTime.now()),
+    Transaction("1003", "JavaScript Book", 4000, DateTime.now()),
+    Transaction("1004", "HDFC Credit Card", 3000, DateTime.now()),
+    Transaction("1005", "Car EMI", 5000, DateTime.now()),
   ];
 
   @override
@@ -32,11 +35,52 @@ class MyHomePage extends StatelessWidget {
               child:
                   Card(color: Colors.blue, child: Text("CHART"), elevation: 5),
             ),
+            Card(
+                child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(decoration: InputDecoration(labelText: "Title")),
+                  TextField(decoration: InputDecoration(labelText: "Amount")),
+                  ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.purple)),
+                    child: Text(
+                      "Add Transaction",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: null,
+                  )
+                ],
+              ),
+            )),
             Column(
               children: transaction.map((tx) {
                 return Card(
-                  child: Text(tx.title),
-                );
+                    child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      padding: EdgeInsets.all(10),
+                      child: Text("\$${tx.amount.toString()}"),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(tx.title.toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.black)),
+                        Text(DateFormat.yMMMd().format(tx.date)),
+                      ],
+                    )
+                  ],
+                ));
               }).toList(),
             )
           ]),
